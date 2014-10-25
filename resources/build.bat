da50e971d9601795a2fa443648ca4882c7f322cb
Wilee999 <gamerdave@einrot.com>
2014-10-25 11:45:56 -0700

    Finish with Phase Files

---
+++ resources/build.bat
@@ -0,0 +1,16 @@
+@echo off
+multify -cf phase_3.mf phase_3
+multify -cf phase_3.5.mf phase_3.5
+multify -cf phase_4.mf phase_4
+multify -cf phase_5.mf phase_5
+multify -cf phase_5.5.mf phase_5.5
+multify -cf phase_6.mf phase_6
+multify -cf phase_7.mf phase_7
+multify -cf phase_8.mf phase_8
+multify -cf phase_9.mf phase_9
+multify -cf phase_10.mf phase_10
+multify -cf phase_11.mf phase_11
+multify -cf phase_12.mf phase_12
+multify -cf phase_13.mf phase_13
+echo BUILT
+echo DO NOT CHECKOUT THE MFS PLZ
